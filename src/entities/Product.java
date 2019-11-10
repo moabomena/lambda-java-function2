fd43d9fa6a95dfb998ec117900237406cb2c7ced
Moab Omena <1moabomena@gmail.com>
2019-11-10 14:31:56 -0300

    Method reference static

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -29,6 +29,13 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
+	}
+	
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
